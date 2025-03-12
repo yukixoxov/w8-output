72803badfc69b78e214721ac4a57a87040e4c51b
yukixoxov <aaapplee@icloud.com>
2025-03-12 23:16:55 +0900

    step9

---
+++ app.js
@@ -0,0 +1,20 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const button = document.getElementById('signIn');
+
+button.addEventListener('click', () => {
+    const inputEmail = email.value;
+    const inputPassword = password.value;
+    localStorage.setItem('email', inputEmail);
+    localStorage.setItem('password', inputPassword);
+});
+
+email.addEventListener('click', () => {
+    const savedEmail = localStorage.getItem('email');
+    email.value = savedEmail;
+});
+
+password.addEventListener('click', () => {
+    const savedPassword = localStorage.getItem('password');
+    password.value = savedPassword;
+});
