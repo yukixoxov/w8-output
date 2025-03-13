496b980c7ee085b60d083875858c1bb310cf6409
yukixoxov <aaapplee@icloud.com>
2025-03-14 02:57:35 +0900

    5

--- login/app.js
+++ login/app.js
@@ -10,12 +10,23 @@ button.addEventListener('click', () => {
     localStorage.setItem('password', inputPassword);
 });
 
-email.addEventListener('click', () => {
+// email.addEventListener('click', () => {
+//     const savedEmail = localStorage.getItem('email');
+//     email.value = savedEmail;
+// });
+
+// password.addEventListener('click', () => {
+//     const savedPassword = localStorage.getItem('password');
+//     password.value = savedPassword;
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    // コードの中身 一般的にはこの方法で画面読み込みと同時に自動入力することが多い
     const savedEmail = localStorage.getItem('email');
     email.value = savedEmail;
-});
 
-password.addEventListener('click', () => {
     const savedPassword = localStorage.getItem('password');
     password.value = savedPassword;
+
 });
